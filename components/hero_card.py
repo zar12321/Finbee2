cf02c1e910834a9baea3f39e5e80f8bcd686f418
zar12321 <ldruszardii@gmail.com>
2026-06-11 06:07:13 +0700

    gajadi

---
+++ components/hero_card.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def render_hero_card(
+    title,
+    subtitle,
+    user_name,
+    emoji="🐝"
+):
+    st.markdown(
+        "<h1>HELLO WORLD</h1>",
+        unsafe_allow_html=True
+    )
