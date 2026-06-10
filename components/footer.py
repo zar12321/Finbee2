ae922f3b1ae30597d01dbf939c61746875e0e59f
zar12321 <ldruszardii@gmail.com>
2026-06-10 19:13:02 +0700

    A

--- components/footer.py
+++ components/footer.py
@@ -3,20 +3,14 @@ import streamlit as st
 from config.settings import APP_VERSION
 
 
+import streamlit as st
+
 def render_footer():
 
     st.markdown(
-        f"""
-        <div class="footer">
-
-            <span>
-                FinBee Personal Finance Dashboard
-            </span>
-
-            <span>
-                Version {APP_VERSION}
-            </span>
-
+        """
+        <div>
+            FinBee Personal Finance Dashboard
         </div>
         """,
         unsafe_allow_html=True
