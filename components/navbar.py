876234622a1d1fe81ad0fadbbe018614a614c696
zar12321 <ldruszardii@gmail.com>
2026-06-10 19:19:25 +0700

    s

--- components/navbar.py
+++ components/navbar.py
@@ -3,18 +3,13 @@ import streamlit as st
 
 import streamlit as st
 
+
 def render_navbar(user_name):
 
     st.markdown(
         f"""
-        <div class="navbar">
-            <div class="navbar-left">
-                🐝 FinBee
-            </div>
-
-            <div class="navbar-right">
-                {user_name}
-            </div>
+        <div>
+            🐝 FinBee | {user_name}
         </div>
         """,
         unsafe_allow_html=True
